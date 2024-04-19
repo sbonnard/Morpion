9bc2cb2154a5252500193471249536b9f1543c3d
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-19 11:52:05 +0200

    add class button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const game = document.getElementById('game');
-
+const cross = 
 
 game.addEventListener('click', function(event){
-    console.log(event.target);
-    const box = event.target;
-    box.style.backgroundColor = "red"
+    // console.log(event.target);
+    // const box = event.target;
+    // box.style.backgroundColor = "red"
+
 });
