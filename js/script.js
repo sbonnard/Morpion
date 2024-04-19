bd49c63ef07e85de60285dec6d32ffb46f77e6a5
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-19 14:08:48 +0200

    created the alternate condition for symbol display

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const buttonArray = document.querySelectorAll('.js-btn');
+let count = 0;
 
 for (const button of buttonArray) {
     button.addEventListener('click', function () {
-        this.classList.add('button--cross');
         this.setAttribute("disabled", "true");
-
-        console.log(button);
+        console.log(count, count % 2);
+        if(count % 2 === 0) {
+            this.classList.add('button--cross');    
+        }else {
+            this.classList.add('button--circle');    
+            
+        }
+        count++;
     });
 }
