61bc955aa78d09cb800711b007772f9f5450b1b6
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-19 11:17:07 +0200

    add symbols static

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ const game = document.getElementById('game');
 
 game.addEventListener('click', function(event){
     console.log(event.target);
-
+    const box = event.target;
+    box.style.backgroundColor = "red"
 });
